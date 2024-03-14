3bcd9afe66c97c5c4e0df7398d3e4f197c3324ee
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-03-14 22:52:24 +0600

    add bookmark

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,15 +1,36 @@
 import PropTypes from 'prop-types';
+import { BsBookmark } from 'react-icons/bs';
 
-const Blog = ({ blog }) => {
-    console.log(blog);
+const Blog = ({ blog, handleAddToBookmark }) => {
+    const { title, cover, reading_time, author, author_img, posted_date, hashtags } = blog;
     return (
-        <div>
-
+        <div className='mb-20'>
+            <img className='w-full mb-8' src={cover} alt={`Cover picture ${title}`} />
+            <div className='flex justify-between mb-4'>
+                <div className='flex'>
+                    <img className='rounded-full w-16' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div className=''>
+                    <span>{reading_time} min read</span>
+                    <button onClick={() => {handleAddToBookmark(blog)}} className='ml-2 text-2xl text-red-600'><BsBookmark></BsBookmark></button>
+                </div>
+            </div>
+            <h2 className="text-4xl">Title: {title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}> <a href="">#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
+    blog: PropTypes.object.isRequired,
+    handleAddToBookmark: PropTypes.func,
 }
 export default Blog;
