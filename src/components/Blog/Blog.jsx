e58a75db23ef9dd23f489409a04bb8c24b4f30d1
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-03-14 23:56:07 +0600

    Add remove function

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { BsBookmark } from 'react-icons/bs';
 
 const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
-    const { title, cover, reading_time, author, author_img, posted_date, hashtags } = blog;
+    const {id, title, cover, reading_time, author, author_img, posted_date, hashtags } = blog;
     return (
         <div className='mb-20 space-y-4'>
             <img className='w-full mb-8' src={cover} alt={`Cover picture ${title}`} />
@@ -25,7 +25,7 @@ const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
                     hashtags.map((hash, idx) => <span key={idx}> <a href="">#{hash}</a></span>)
                 }
             </p>
-            <button onClick={() => { handleMarkAsRead(reading_time) }} className='text-blue-700 font-bold underline'>Mark as Read</button>
+            <button onClick={() => { handleMarkAsRead(id, reading_time) }} className='text-blue-700 font-bold underline'>Mark as Read</button>
         </div>
     );
 };
